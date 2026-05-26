178b3eb0992e942c3bca306e014165924ab34101
joaquimedina <joaqmovil@gmail.com>
2026-05-26 21:17:06 +0200

    5

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -54,7 +54,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Psicología y Artes Escénicas',
-        text: "Psicólogo y profesional multidisciplinar especializado en intervención comunitaria, participación social y proyectos culturales con impacto social. Combino psicología, artes escénicas, creación audiovisual y tecnologías digitales para diseñar iniciativas que favorecen el bienestar, la inclusión, el envejecimiento activo y la construcción de comunidades más humanas y participativas. [*Ver programa.*](/escena)",
+        text: "Psicólogo y profesional multidisciplinar especializado en intervención comunitaria, participación social y proyectos culturales con impacto social. Combino psicología, artes escénicas, creación audiovisual y tecnologías digitales para diseñar iniciativas que favorecen el bienestar, la inclusión, el envejecimiento activo y la construcción de comunidades más humanas y participativas. [*Ver programa*](/escena)",
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
