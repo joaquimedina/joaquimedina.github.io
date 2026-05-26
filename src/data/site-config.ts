b4e7e796615c69a577c43cf6fcf75766bafb0184
Temsuo <quomhum@gmail.com>
2026-05-26 19:12:00 +0200

    Update site-config.ts

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -3,7 +3,7 @@ import hero from '../assets/images/hero.jpg';
 import type { SiteConfig } from '../types';
 
 const siteConfig: SiteConfig = {
-    website: 'https://temsuo.github.io/',
+    website: 'https://joaquimedina.github.io/',
     
     avatar: {
         src: avatar,
