a5d19f1c03b75e63a1bc9893585e5d0ce03c16f9
joaquimedina <joaqmovil@gmail.com>
2026-05-27 16:46:22 +0200

    5

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -14,7 +14,7 @@ const siteConfig: SiteConfig = {
     description: 'Portfolio Joaquín Medina, psicología y arteterapia',
     image: {
         src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        alt: 'Portfolio Joaquín Medina, psicología, intervención social y artes escénicas'
     },
     headerNavLinks: [
         {
@@ -54,7 +54,7 @@ const siteConfig: SiteConfig = {
     ],
     hero: {
         title: 'Psicología y Artes Escénicas',
-        text: "Psicólogo y profesional multidisciplinar especializado en intervención comunitaria, participación social y proyectos culturales con impacto social. Combino psicología, artes escénicas, creación audiovisual y tecnologías digitales para diseñar iniciativas que favorecen el bienestar, la inclusión, el envejecimiento activo y la construcción de comunidades más humanas y participativas. [*Ver programa*](/escena)",
+        text: "Psicólogo y profesional multidisciplinar especializado en intervención comunitaria, participación social y proyectos culturales con impacto social. Combino psicología, artes escénicas, creación audiovisual y tecnologías digitales para diseñar iniciativas que favorecen el bienestar, la inclusión, un envejecimiento activo y saludable y la construcción de comunidades más humanas y participativas. [*Ver programa*](/escena)",
         image: {
             src: hero,
             alt: 'A person sitting at a desk in front of a computer'
