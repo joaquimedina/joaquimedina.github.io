a0f00377de62df3e270a5f616a0d2a06b19563f1
joaquimedina <joaqmovil@gmail.com>
2026-05-27 20:34:26 +0200

    j

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://temsuo.github.io/',
+    site: 'https://joaquimedina.github.io/',
 
     
     vite: {
